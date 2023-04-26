3535650cdb7a2849fcbe2d3e711c87bc6dc57938
yNsLc <104109451+yNsLc@users.noreply.github.com>
2023-04-26 20:03:08 -0300

    Create README.m

---
+++ README.m
@@ -0,0 +1,14 @@
+### Olá! Meu nome é Lucas Felicio
+
+[![LinkeIn](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)]()
+[![Discord](https://img.shields.io/badge/Discord-7289DA?style=for-the-badge&logo=discord&logoColor=white)]()
+
+[![Anurag's GitHub stats](https://github-readme-stats.vercel.app/api?username=gr-Soares&theme=radical)](https://github.com/anuraghazra/github-readme-stats)
+
+## Tecnologias que venho Estudando!
+
+<div style="display: inline_block">
+  <img align="center" alt="C++" src="https://img.shields.io/badge/C%2B%2B-00599C?style=for-the-badge&logo=c%2B%2B&logoColor=white" />
+  <img align="center" alt="C#" src="https://img.shields.io/badge/C%23-239120?style=for-the-badge&logo=c-sharp&logoColor=white" />
+  <img align="center" alt=".NET" src="https://img.shields.io/badge/.NET-5C2D91?style=for-the-badge&logo=.net&logoColor=white" />
+</div><br/>
